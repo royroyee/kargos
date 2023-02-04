84354aed4be8c9b75ca1c366a56073866c9f96e4
HWAN <kim131325@naver.com>
2023-02-04 18:08:07 +0900

    Init module

---
+++ common/common.go
@@ -0,0 +1,77 @@
+package common
+
+// Overview main
+type Home struct {
+	Version    string // kuberentes version
+	TotalNodes int32  // total nodes
+	Created    int32  // created
+
+	Tabs map[string]int32 // total_resources ~ daemon_sets
+
+	TopNamespaces []string
+	AlertCount    int32 // warning 등의 이벤트만
+}
+
+// Alert
+type Alert struct {
+	tag     string
+	message string
+	uuid    string
+}
+
+// Node
+type Node struct {
+	Name      string
+	CpuUsage  float64
+	RamUsage  float64
+	DiskUsage float64
+	IP        string
+	OsImage   string
+	Pods      []Pod
+
+	// detail 항목들
+}
+
+// Pod
+type Pod struct {
+	Name             string
+	Status           string // Running  or Pending
+	Image            string
+	Age              string // created
+	Namespace        string `json:"namespace"`
+	PodIP            string `json:"podIP"`
+	ServiceConnected *bool  `json:"serviceConnected"`
+	Restarts         int32  `json:"restarts"`
+}
+
+// Deployment
+type Deployment struct {
+	Name      string `json:"name"`
+	Namespace string `json:"namespace"`
+	Image     string
+	Status    string
+	Label     string
+	created   string `json:"pod_count"`
+
+	// detail 항목들
+}
+
+// Namespace
+type Namespace struct {
+	Name      string
+	Status    string
+	Cpu_usage string
+	Ram_usage string
+
+	// Infra agent
+	process []Process // inner struct
+}
+
+// Process (Infra agent)
+type Process struct {
+	Name     string
+	Status   string
+	PID      int32
+	CpuUsage int32
+	RamUsage int32
+}
